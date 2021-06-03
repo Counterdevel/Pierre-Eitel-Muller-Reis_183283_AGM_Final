d914dcb681754310affb3065238f928a77e5fc5a
Pierre Muller <pierreeitel@hotmail.com>
2021-06-03 12:00:39 -0300

    FloatingActionButton, refatoração dos nomes e criação xml do formulario criados.

--- app/src/main/java/com/example/agenda/ui/activities/ListaPessoasActivity.java
+++ app/src/main/java/com/example/agenda/ui/activities/ListaPessoasActivity.java
@@ -1,22 +1,23 @@
-package com.example.agenda;
+package com.example.agenda.ui.activities;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.agenda.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaPessoasActivity extends AppCompatActivity {
 
     @Override                                                                                                       //@Override: sobreescreve superclasses dentro da IDE.
     protected void onCreate(Bundle savedInstanceState){                                                             //onCreate: aqui é onde fica a maior parte da inicialização.
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_pessoas);                                                            //Busca o layout no projeto.
 
         List<String> pessoa = new ArrayList<>(Arrays.asList("Pierre", "Gabriela"," Bruna"));                        //Um array para criar a lista.
 
